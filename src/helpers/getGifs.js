f3385fbf54ea7acb65f06220836628ef281016f3
sdiazbarraza <sebastiandesarrolla@gmail.com>
2024-11-18 18:15:18 +0000

    remove apikey

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=r82QcRvDlVHJFznTAXj8Q8nR9ZOD18wR&rating=g`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=${APIKEY}&rating=g`;
     const resp = await fetch(url);
     const {data} = await resp.json();
     const gifs = data.map(img => ({
